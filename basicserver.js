48bfe49c016bb4bc86943a4cef6cc49ce1c7fcc5
Shweta Patel <shweta18082000@gmail.com>
2022-08-15 11:21:48 +0530

    login for using fs

--- basicserver.js
+++ basicserver.js
@@ -1,9 +1,13 @@
-let http=require("http")
-
+let http=require("http")//internal
+let fs=require("fs")
 http.createServer(function(req,res){
     console.log("request...."+req.url)
   
 if(req.url=="/signup"){
+
+    let loginFile=fs.readFileSync("login.html")
+    res.writeHead(200,{"Content-Type":"text/html"})
+    res.write(loginFile);
     res.write("signup");
 }else if(req.url=="/favicon.ico"){
     res.write("differnt");
